b99658ea0345c01485ee6b4eea2394ccb3905346
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-05-02 12:48:40 -0700

    More tests.

--- tests/test_baduk_id.py
+++ tests/test_baduk_id.py
@@ -1,7 +1,20 @@
 import unittest
 
-from baduk_id.baduk_id import encode
+from baduk_id.baduk_id import encode, decode
 
+
+# Eventually, other board sizes will be supported
+N = 361
+
+EMPTY_BOARD = [0] * N
+
+# Technically illegal positions, but useful for testing
+ALL_BLACK = [1] * N
+ALL_WHITE = [2] * N
+
+# Example Joseki
+# It would be cumbersome to calculate IDs for these by hand, but still useful
+# to test in order to detect unexpected changes in behavior.
 FLYING_KNIFE = (
     [0] * 19
     + [0] * 12 + [1, 2, 0, 0, 0, 0, 0]
@@ -10,29 +23,30 @@ FLYING_KNIFE = (
     + [0] * 12 + [0, 0, 1, 2, 0, 0, 0]
     + [0] * 19 * 14)
 
-# Eventually, other board sizes will be supported
-N = 361
-
 class TestEncode(unittest.TestCase):
     def test_empty(self):
-        empty_board = [0] * N
-        self.assertEqual(encode(empty_board), 0)
+        self.assertEqual(encode(EMPTY_BOARD), 0)
 
     def test_full(self):
-        # techically, these are illegal positions, but for testing purposes
-        # it is good to check
-
-        all_black = [1] * N
-        all_white = [2] * N
-
-        self.assertEqual(encode(all_black), 3**N - 2**N)
-        self.assertEqual(encode(all_white), 3**N - 1)
+        self.assertEqual(encode(ALL_BLACK), 3**N - 2**N)
+        self.assertEqual(encode(ALL_WHITE), 3**N - 1)
 
     def test_random_examples(self):
-        """
-        Tests some well known Joseki.  While it's hard to calculate these
-        IDs by hand, I wanted to include some real-world tests to detect
-        random ID changes.
-        """
-
         self.assertEqual(encode(FLYING_KNIFE), 23785089243857643089678644)
+
+    def test_reversability(self):
+        # Obviously we can't test every possible board state...
+        # so we just test the first million
+        for i in range(1000000):
+            self.assertEqual(encode(decode(i)), i)
+
+class TestDecode(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(decode(0), EMPTY_BOARD)
+
+    def test_full(self):
+        self.assertEqual(decode(3**N - 2**N), ALL_BLACK)
+        self.assertEqual(decode(3**N - 1), ALL_WHITE)
+
+    def test_examples(self):
+        assertEqual(decode(23785089243857643089678644), FLYING_KNIFE)
