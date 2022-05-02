4c231a6cd77b1c432ec67f9a46b7e8397de08eab
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-05-02 14:36:34 -0700

    Reduce reversability test to 1000.

--- tests/test_baduk_id.py
+++ tests/test_baduk_id.py
@@ -37,10 +37,8 @@ class TestEncode(unittest.TestCase):
     def test_reversability(self):
         # Obviously we can't test every possible board state...
         # so we just test the first million
-        for i in range(10000):
+        for i in range(1000):
             self.assertEqual(encode(decode(i)), i)
-    def test_thou(self):
-        self.assertEqual(encode(decode(1000)), 1000)
 
 class TestDecode(unittest.TestCase):
     def test_empty(self):
